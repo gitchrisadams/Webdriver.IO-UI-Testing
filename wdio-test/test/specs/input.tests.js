f92918ff7b646458d58812a94f8bf28c4270ac39
Christopher Adams <chrismichaeladams@gmail.com>
2019-06-23 15:24:51 -0400

    Test working for dropdowns

--- wdio-test/test/specs/input.tests.js
+++ wdio-test/test/specs/input.tests.js
@@ -2,54 +2,29 @@ import assert from 'assert';
 import {expect} from 'chai';
 
 describe('Checking inputs', function() {
-  // Specify retries to test a failing it:
-  //this.retries(2);
 
   before(() => {
     // Run before all tests start.
     browser.url('http://localhost:3000');
   });
 
-  /* Test checkbox input: */
-  // it(`Test checkbox inputs`, () => {
-  //   // Get the firt checkbox:
-  //   const checkbox_one = $(`[value="Banana"]`);
-  //   expect(checkbox_one.isSelected()).to.be.false;
-  //   checkbox_one.click();
-  //   expect(checkbox_one.isSelected()).to.be.true;
-  //   browser.pause(2000);
-  //   checkbox_one.click();
-  //   browser.pause(2000);
-  // });
-
   /* Testing dropdowns */
   it(`Test dropdowns`, () => {
-    // $(`#dropdowns`).selectByVisibleText(`pepper`);
+    // Data expected: 
+    const fruitOptionsExpected = ['Banana', 'Orange'];
 
-    // expect($(`#dropdowns`).getValue()).to.equal(`pepper`);
+    // Get the Select box and click it to open it.
     const fruitDropdownEl = $(`#fruit-select`);
     fruitDropdownEl.click();
-    const item1 = $('#react-select-2-option-0');
-    const item2 = $('#react-select-2-option-1');
 
-
-    // console.log('item1', item1.getText());
-    // console.log('item2', item2.getText());
-
+    // Get all the dropdown items
     const allItems = $$(`.css-1ml51p6-MenuList`);
-    //console.log('allItems', allItems);
+    const listSplit = allItems[0].getText().split('\n');
 
-    const temp = allItems.map((el) => {
-      // console.log('loop');
-      // console.log('element: ' + el.getText());
-      return el.getText();
+    // Loop through dropdown items and compare to data expected:
+    listSplit.forEach((item) => {
+      expect(fruitOptionsExpected.includes(item)).to.equal(true);
     });
-    console.log('temp', temp);
-
-    const listSplit = temp[0].split('\n');
-
-    console.log('listsplit', listSplit);
-
 
     browser.pause(3000);
   });
